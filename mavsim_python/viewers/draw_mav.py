20fb6e7bec635be150ae2078e1ec8bc050b0baeb
Rob Jones <rob.jones@motion-impossible.com>
2023-10-21 12:37:42 +0100

    chapter 2 exercise done

--- mavsim_python/viewers/draw_mav.py
+++ mavsim_python/viewers/draw_mav.py
@@ -86,22 +86,41 @@ class DrawMav:
             Points that define the mav, and the colors of the triangular mesh
             Define the points on the aircraft following diagram in Figure C.3
         """
-        ##### TODO #####
         # define MAV body parameters
-        #fuse_h = 
+        fuse_h = 3
+        fuse_w = 2
+        fuse_l1 = 4
+        fuse_l2 = 2
+        wing_l = 4
+        wing_w = 12
+        fuse_l3 = 12
+        tailwing_l = 2.5
+        tailwing_w = 6
+        tail_h = 5
 
         # Define the points on the aircraft following diagram Fig 2.14
         # points are in NED coordinates
-        ##### TODO #####
-        points = np.array([[0, 0, 0],  # point 1 [0]
-                           [1, 1, 1],  # point 2 [1]
-                           [1, 1, 0],  # point 3 [2]
+        points = np.array([[fuse_l1,                0,                0],             # point 1 [0]
+                           [fuse_l2,                0.5 * fuse_w,    -0.5 * fuse_h],  # point 2 [1]
+                           [fuse_l2,               -0.5 * fuse_w,    -0.5 * fuse_h],  # point 3 [2]
+                           [fuse_l2,               -0.5 * fuse_w,     0.5 * fuse_h],  # point 4
+                           [fuse_l2,                0.5 * fuse_w,     0.5 * fuse_h],  # point 5
+                           [-fuse_l3,               0,                0],             # point 6
+                           [0,                      0.5 * wing_w,     0],             # point 7
+                           [-wing_l,                0.5 * wing_w,     0],             # point 8
+                           [-wing_l,               -0.5 * wing_w,     0],             # point 9
+                           [0,                     -0.5 * wing_w,     0],             # point 10
+                           [-fuse_l3 + tailwing_l,  0.5 * tailwing_w, 0],             # point 11
+                           [-fuse_l3,               0.5 * tailwing_w, 0],             # point 12
+                           [-fuse_l3,              -0.5 * tailwing_w, 0],             # point 13
+                           [-fuse_l3 + tailwing_l, -0.5 * tailwing_w, 0],             # point 14
+                           [-fuse_l3 + tailwing_l,  0,                0],             # point 15
+                           [-fuse_l3,               0,               -tail_h]         # point 16
                            ]).T
 
         # scale points for better rendering
-        scale = 20
+        scale = 5
         points = scale * points
-
         #   define the colors for each face of triangular mesh
         red = np.array([1., 0., 0., 1])
         green = np.array([0., 1., 0., 1])
@@ -110,8 +129,13 @@ class DrawMav:
         meshColors = np.empty((13, 3, 4), dtype=np.float32)
 
         # Assign colors for each mesh section
-        ##### TODO #####
-        meshColors[0] = yellow # nose-top
+        for i in range(4):
+            meshColors[i] = yellow
+        for i in range(4, 8):
+            meshColors[i] = blue
+        for i in range(8, 12):
+            meshColors[i] = green
+        meshColors[12] = red
 
         return points, meshColors
 
@@ -124,7 +148,19 @@ class DrawMav:
         points = points.T
 
         #Define each section of the mesh with 3 points
-        ##### TODO #####
-        mesh = np.array([[points[0], points[1], points[2]]]) # nose-top
-
+        mesh = np.array([
+            [points[0], points[1], points[2]],     # nose top
+            [points[0], points[2], points[3]],     # nose west
+            [points[0], points[3], points[4]],     # nose bottom
+            [points[0], points[1], points[4]],     # nose east
+            [points[1], points[2], points[5]],     # body top
+            [points[2], points[3], points[5]],     # body west
+            [points[3], points[4], points[5]],     # body bottom
+            [points[1], points[4], points[5]],     # body east
+            [points[6], points[7], points[9]],     # wing a
+            [points[8], points[9], points[7]],     # wing b
+            [points[10], points[11], points[13]],  # tail wing a
+            [points[11], points[12], points[13]],  # tail wing b
+            [points[14], points[5], points[15]],   # tail
+        ])
         return mesh
